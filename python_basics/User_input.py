ade2c97e52968dab97b9b754c7416f627c52c2ba
Njane01 <alvinnjane92@gmail.com>
2026-02-11 10:39:58 +0300

    input

---
+++ python_basics/User_input.py
@@ -0,0 +1,9 @@
+# Name: Njane Alvin
+# Date: 11.02.2026
+
+
+# Program to get user input
+
+name= input("What is you name?")
+
+print(name)
---
+++ python_basics/numbers.py
@@ -0,0 +1,21 @@
+# Name: Njane Alvin
+# Date: 11.02.2026
+
+
+# Program to add,subtract, divide, multiply numbers
+
+first_number= int(input("enter the first number"))
+
+second_number= int(input("enter the second number"))
+
+sum_number= first_number + second_number
+difference_number= first_number-second_number
+quotient_number= first_number / second_number
+product_number= first_number * second_number
+
+
+print(f"the sum of numbers : {sum_number}")
+ 
+print(f"the difference of numbers : {difference_number}")
+print(f"the quotient of numbers : {quotient_number}")
+print(f"the product of numbers : {product_number}")     
