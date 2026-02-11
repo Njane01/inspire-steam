aaf457a3ab357f5bb8fb71ee2b9a937b7225d71e
Njane01 <alvinnjane92@gmail.com>
2026-02-11 09:18:14 +0300

    first commit

---
+++ python_basics/challenge1.py
@@ -0,0 +1,11 @@
+University= "JKUAT"
+Grade= "A"
+Favorite_course= "Bachelors Degree in Nursing"
+Cluster_weight= "40"
+
+
+
+print(f"My favorite university is {University}")
+print(f"I attained grade {Grade}")
+print(f"Favorite course is {Favorite_course}")
+print(f"i attained a cluster weight of {Cluster_weight}")
---
+++ python_basics/hello.py
@@ -0,0 +1,14 @@
+# This is a comment
+print ("hello from Alvin")
+# city
+print("I am from Kenol")
+# my hobbies
+print("I like to play the guitar,watching movies and play around with musical instruments")
+
+age = 25
+height = 1.7 
+weight = 60
+
+print(f" I am {age} years old")
+print(f" I weigh {weight} kilograms")
+print(f" I am also {height} tall")
