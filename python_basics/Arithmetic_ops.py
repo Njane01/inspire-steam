69f71906ab2ccbe18a44b6099d58730f975fb597
alvinnjane <alvinnjane92@gmail.com>
2026-02-17 12:59:29 +0300

    Add files via upload

---
+++ python_basics/Arithmetic_ops.py
@@ -0,0 +1,33 @@
+# Njane Alvin
+#17.02.2026
+#program to perform arithmetic operations
+
+
+
+
+f_number = 12
+s_number= 34
+sum_number= f_number+s_number
+difference_number= f_number-s_number
+product_number= f_number*s_number
+quotient_number=f_number/s_number
+
+print ("the sum of the numbers %d" %sum_number)
+print ("the difference of the numbers %d" %difference_number)
+print ("the product of the numbers %d" %product_number)
+print ("the quotient of the numbers %f" %quotient_number)
+
+
+# Modulus
+
+print(7%5)
+
+#Even and odd numbers
+for x in range(0,21):
+    
+    print(x)
+    if x%2 == 0:
+        print("Even Number")
+    else:
+        print("Odd number")
+
---
+++ python_basics/Break.py
@@ -0,0 +1,24 @@
+#Njane Alvin
+#17.02.2026
+# Program to illustraate break in python
+
+number=1
+while number<10:
+    print(number)
+    number+=1
+
+    
+
+    if number ==4 :
+             print('Loop has been broken')
+             break # breaks numbers when it gets to 4
+            
+    if number==3:
+           continue
+    print(number)
+    
+
+
+
+
+
